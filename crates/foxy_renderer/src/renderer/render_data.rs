7ec1d4df51c382c533764ab20ad31fc3a1fce8f4
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-01-24 22:02:43 -0500

    Update render_data.rs

--- crates/foxy_renderer/src/renderer/render_data.rs
+++ crates/foxy_renderer/src/renderer/render_data.rs
@@ -1,12 +1,7 @@
 use std::fmt::Debug;
 
+#[derive(Default)]
 pub struct RenderData {}
-
-impl Default for RenderData {
-  fn default() -> Self {
-    Self {}
-  }
-}
 
 impl Debug for RenderData {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
