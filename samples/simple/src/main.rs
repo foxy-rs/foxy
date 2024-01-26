767bcb16d52a4b104b1b0a3bf1a003c6c1cffa2c
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-01-26 10:39:40 -0500

    Update main.rs

--- samples/simple/src/main.rs
+++ samples/simple/src/main.rs
@@ -11,10 +11,17 @@ fn main() {
     .with_size(800, 450)
     .build_unwrap();
 
+  let mut x: u32 = 0;
+
   for stage in lifecycle {
     match stage {
-      Stage::FixedUpdate { .. } => debug!("FixedUpdate"),
-      Stage::Update { .. } => debug!("Update"),
+      Stage::FixedUpdate { .. } => {
+        x = x.wrapping_add(1);
+        debug!("FixedUpdate")
+      },
+      Stage::Update { .. } => {
+        debug!("Update {x}")
+      },
       _ => {}
     }
   }
