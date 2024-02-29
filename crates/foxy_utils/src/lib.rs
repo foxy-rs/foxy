d05107b633708bfc9b039979d31fbecfb88adca8
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-02-29 18:28:35 -0500

    Removed crates

--- crates/foxy_utils/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![deny(unsafe_op_in_unsafe_fn)]
-
-pub mod handle;
-pub mod mailbox;
-pub mod thread;
--- crates/foxy_window/src/window/stage.rs
+++
@@ -1,9 +0,0 @@
-use strum::Display;
-
-// KEEP THESE SMALL since you need to clone them for each iteration
-#[derive(Display, Clone, Copy)]
-pub enum Stage {
-  Looping,
-  Exiting,
-  ExitLoop,
-}
