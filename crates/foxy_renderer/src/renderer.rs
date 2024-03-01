3ea868f9c7b54fbb9c39960514e014ee7da9232e
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-03-01 07:53:29 -0500

    Device selection

--- crates/foxy_renderer/src/renderer.rs
+++ crates/foxy_renderer/src/renderer.rs
@@ -2,11 +2,20 @@ use std::sync::Arc;
 
 use ezwin::prelude::{Message, Window};
 use foxy_time::Time;
+use vulkano::{
+  instance::{Instance, InstanceCreateInfo},
+  swapchain::Surface,
+  VulkanLibrary,
+};
 
-use self::render_data::RenderData;
+use self::{device::FoxyDevice, instance::FoxyInstance, render_data::RenderData};
 use crate::error::RendererError;
 
+mod debug;
+mod device;
+mod instance;
 pub mod render_data;
+mod shader;
 
 // pub struct Egui {
 //   context: egui::Context,
@@ -14,11 +23,24 @@ pub mod render_data;
 //   state: egui_winit::State,
 // }
 
-pub struct Renderer {}
+pub struct Renderer {
+  is_dirty: bool,
+  instance: FoxyInstance,
+  surface: Arc<Surface>,
+  device: FoxyDevice,
+}
 
 impl Renderer {
   pub fn new(window: Arc<Window>) -> Result<Self, RendererError> {
-    Ok(Self {})
+    let instance = FoxyInstance::new(&window)?;
+    let surface = Surface::from_window(instance.vk().clone(), window)?;
+    let device = FoxyDevice::new(instance.vk().clone(), surface.clone())?;
+    Ok(Self {
+      is_dirty: false,
+      instance,
+      surface,
+      device,
+    })
   }
 
   pub fn delete(&mut self) {}
@@ -29,7 +51,7 @@ impl Renderer {
 
   pub fn resize(&mut self) {}
 
-  pub fn input(&mut self, _message: &Message) -> bool {
+  pub fn input(&mut self, message: &Message) -> bool {
     false
   }
 }
